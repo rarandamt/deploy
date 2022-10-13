b98911a2cadc61bd42a776e797c64c1abd2cf85a
rarandamt <91378350+rarandamt@users.noreply.github.com>
2022-10-13 09:43:00 -0500

    Update ExampleApplication.java

--- src/main/java/co/edu/usa/example/ExampleApplication.java
+++ src/main/java/co/edu/usa/example/ExampleApplication.java
@@ -6,11 +6,15 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class ExampleApplication extends SpringBootServletInitializer{
 
-
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ExampleApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
