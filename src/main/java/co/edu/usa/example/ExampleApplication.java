1fb6ed3d7b0c2313e4116c7279222e4eb2c1ad88
rarandamt <91378350+rarandamt@users.noreply.github.com>
2022-10-13 12:15:13 -0500

    Update ExampleApplication.java

--- src/main/java/co/edu/usa/example/ExampleApplication.java
+++ src/main/java/co/edu/usa/example/ExampleApplication.java
@@ -1,15 +1,14 @@
 package co.edu.usa.example;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.ApplicationRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @SpringBootApplication
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "REDIS")
 public class ExampleApplication extends SpringBootServletInitializer{
 
 @Override
